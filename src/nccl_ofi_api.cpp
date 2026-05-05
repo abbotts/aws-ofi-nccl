231b5d085943da850e8dc2b54b83f0b643bcf199
Abbott, Stephen R <abbottsr@ornl.gov>
2026-05-05 10:35:28 -0400

    Fix a misunderstanding about the size arg

--- src/nccl_ofi_api.cpp
+++ src/nccl_ofi_api.cpp
@@ -588,17 +588,20 @@ ncclResult_t nccl_net_ofi_test(void* req, int* done, int* size)
 		if (auto it = request_map.find(req); it != request_map.end()) {
 			
 			void *data = it->second;
-			
 			if (auto known_it = known_buffers.find(data); known_it != known_buffers.end()) {			
-				uint32_t *data_as_u32 = (uint32_t *)data;
-				uint32_t *known_data_as_u32 = (uint32_t *)known_it->second.first;
+
+                uint32_t *data_as_u32 = (uint32_t *)data;
+                uint32_t *known_data_as_u32 = (uint32_t *)known_it->second.first;
+
+                // We need to use the stored size because the size input pointer could be NULL
+                size_t known_size = known_it->second.second;
 			
-				for (size_t i = 0; i < size / sizeof(uint32_t); i++) {
-					if (data_as_u32[i] != known_data_as_u32[i]) {
-				        NCCL_OFI_WARN("Buffer %p with size %zu has different data at request completion - index: %zu, new data: %08x, old data: %08x",
-						      data, size, i, data_as_u32[i], known_data_as_u32[i]);
-					}
-				}
+                for (size_t i = 0; i < known_size / sizeof(uint32_t); i++) {
+                    if (data_as_u32[i] != known_data_as_u32[i]) {
+                        NCCL_OFI_WARN("Buffer %p with size %zu has different data at request completion - index: %zu, new data: %08x, old data: %08x",
+                                      data, known_size, i, data_as_u32[i], known_data_as_u32[i]);
+                    }
+                }
 			}
 		}
 		// Erase the request from the map, since it's completed
